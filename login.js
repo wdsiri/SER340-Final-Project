58981783ecbf435b2074a8dd41d121f711332a92
Billy Siri <wdsiri@quinnipiac.edu>
2022-12-06 20:10:12 -0500

    implemented custom splash pages

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function onClick()
 {
-    if(document.getElementById("student").checked) document.getElementById('loginbtn').href = "courses.html";
-    else if (document.getElementById("prof").checked) document.getElementById('loginbtn').href = "coursesProf.html";
+    if(document.getElementById("student").checked) document.getElementById('loginbtn').href = "splash.html";
+    else if (document.getElementById("prof").checked) document.getElementById('loginbtn').href = "splashProf.html";
 }
