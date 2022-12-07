376def56488b2e87b84aa5248e2109931149bc9a
aidenrosen <aidenrrosen@gmail.com>
2022-12-06 20:35:50 -0500

    Added admin functionality

--- login.js
+++ login.js
@@ -2,4 +2,5 @@ function onClick()
 {
     if(document.getElementById("student").checked) document.getElementById('loginbtn').href = "splash.html";
     else if (document.getElementById("prof").checked) document.getElementById('loginbtn').href = "splashProf.html";
+    else if (document.getElementById("admin").checked) document.getElementById('loginbtn').href = "createAccount.html";
 }
