534845b7c903843a17f8cd17b57f455c8c7a6b1e
Billy Siri <wdsiri@quinnipiac.edu>
2022-12-07 12:43:08 -0500

    styling changes and nav

--- login.js
+++ login.js
@@ -27,5 +27,5 @@ function navigate()
 {
     if(document.getElementById("student").checked) document.getElementById('loginbtn').href = "splash.html";
     else if (document.getElementById("prof").checked) document.getElementById('loginbtn').href = "splashProf.html";
-    else if (document.getElementById("admin").checked) document.getElementById('loginbtn').href = "createAccount.html";
+    else if (document.getElementById("admin").checked) document.getElementById('loginbtn').href = "createAccountAdmin.html";
 }
