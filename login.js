9ec10763e15621a5f8507e6d380b95cae2cdf17b
aidenrosen <aidenrrosen@gmail.com>
2022-12-06 20:58:19 -0500

    Fixed validation

--- login.js
+++ login.js
@@ -1,5 +1,30 @@
 function onClick()
 {
+    var emailVal, passVal = false;
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+   if(!document.getElementById("exampleInputEmail1").value.match(validRegex))
+   {
+    alert("Invalid email address");
+    document.getElementById("exampleInputEmail1").focus;
+    return false;
+   }
+   else emailVal = true;
+   
+   
+   if(document.getElementById("exampleInputPassword1").value.length < 8 )
+   {
+    alert("Invalid password");
+    document.getElementById("exampleInputPassword1").focus;
+    return false;
+   }
+   else passVal = true;
+
+   if(emailVal && passVal) navigate();
+   
+}
+
+function navigate()
+{
     if(document.getElementById("student").checked) document.getElementById('loginbtn').href = "splash.html";
     else if (document.getElementById("prof").checked) document.getElementById('loginbtn').href = "splashProf.html";
     else if (document.getElementById("admin").checked) document.getElementById('loginbtn').href = "createAccount.html";
